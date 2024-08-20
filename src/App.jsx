175aa8aad7b15530d6d709110af2d5ec40affca5
hamesomer11 <hamzaomer45@gmail.com>
2024-08-20 22:11:51 +0100

    Login to frontend: Exercise 5.2

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ const App = () => {
   const [blogs, setBlogs] = useState([])
   const [username, setUsername] = useState('') 
   const [password, setPassword] = useState('')
+  const [errorMessage, setErrorMessage] = useState(null)
   const [user, setUser] = useState(null)
 
   useEffect(() => {
@@ -46,6 +47,15 @@ const App = () => {
     }
   }
 
+  const handleLogOut = () => {
+    window.localStorage.removeItem(
+      'loggedBlogappUser', JSON.stringify(user)
+    )
+    setUser(null)
+    setUsername('')
+    setPassword('')
+  }
+
   if (user === null) {
     return (
         <div>
@@ -78,7 +88,7 @@ const App = () => {
   return (
     <div>
       <h2>blogs</h2>
-      <p>{user.name} logged-in</p>
+      <p>{user.name} logged-in <button onClick={handleLogOut}>logout</button> </p>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
