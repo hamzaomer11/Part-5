41a5ff57c4dd5f90daf0093fe4b1f5d39386412b
hamesomer11 <hamzaomer45@gmail.com>
2024-08-22 23:27:00 +0100

    Login to frontend: Exercise 5.8 - commit #1

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react"
+import blogService from '../services/blogs'
 
-const Blog = ({ blog }) => {
+const Blog = ({ blog, updateObject }) => {
   const blogStyle = {
     paddingTop: 10,
     paddingLeft: 2,
@@ -10,12 +11,26 @@ const Blog = ({ blog }) => {
   }
 
   const [visible, setVisible] = useState(false)
+  const [blogs, setBlogs] = useState([])
 
   const hideWhenVisible = { display: visible ? 'none' : '' }
   const showWhenVisible = { display: visible ? '' : 'none' }
 
   const toggleVisibility = () => {
     setVisible(!visible)
+  }
+
+  const updateBlog = () => {
+    updateObject = {
+      likes: 1
+    }
+    console.log(blog.id)
+    blogService
+      .update(blog.id, updateObject)
+        .then(returnedBlog => {
+          console.log(returnedBlog, 'response')
+        setBlogs(blogs.concat(returnedBlog))
+      })
   }
 
   return (
@@ -29,7 +44,7 @@ const Blog = ({ blog }) => {
         <br />
         {blog.url} 
         <br />
-        likes {blog.likes} <button>like</button>
+        likes {blog.likes} <button onClick={updateBlog}>like</button>
         <br />
         {blog.author}
       </div>
