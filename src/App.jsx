d3f330c376bb691091675459860f11b7d206e429
hamesomer11 <hamzaomer45@gmail.com>
2024-08-25 19:06:13 +0100

    Login to frontend: Exercise 5.11 - commit #7

--- src/App.jsx
+++ src/App.jsx
@@ -145,7 +145,7 @@ const App = () => {
       <br />
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} updateBlog={updateBlog} 
-        deleteBlog={deleteBlog} canUserDelete={user.id === blog.user.id} />
+        deleteBlog={deleteBlog} canUserDelete={user.username === blog.user.username} />
       )}
     </div>
   )
